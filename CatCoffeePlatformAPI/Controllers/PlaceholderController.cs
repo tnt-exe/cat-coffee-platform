91a5ac0ab9ef752306cb0f1a4308234cce8ef9ad
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-01-22 06:48:41 +0700

    chore: controller placeholder

---
+++ CatCoffeePlatformAPI/Controllers/PlaceholderController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CatCoffeePlatformAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlaceholderController : ControllerBase
+    {
+    }
+}
