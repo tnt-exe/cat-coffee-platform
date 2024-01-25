1bf2a79f1bff7f45fcef014da4ad7c18e00cf8c6
danialtien <tientung2029901@gmail.com>
2024-01-25 21:03:00 +0700

    add baseRepo

---
+++ DAO/Helper/ErrorCode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO.Helper
+{
+    public enum ErrorCode
+    {
+        BadRequest = 400,
+        NotFound = 404,
+        ServerError = 500,
+        UnAuthorize = 401,
+        Forbidden = 403,
+        UnknownError = 999
+    }
+}
---
+++ Repository/Implement/BaseRepo.cs
@@ -0,0 +1,47 @@
+﻿using DAO.Helper;
+using Repository.Interface;
+using System.Linq.Expressions;
+
+namespace Repository.Implement
+{
+    public abstract class BaseRepo<TRequest, TResponse> : IBaseRepo<TRequest, TResponse> where TRequest : class where TResponse : class
+    {
+
+        public BaseRepo() { }
+
+        public Task<OperationResult<TResponse>> Create(TRequest requestModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult<bool>> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult<Pagination<TResponse>>> GetAccountPaginationAsync(int pageIndex = 0, int pageSize = 10)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult<IEnumerable<TResponse>>> GetAll(Expression<Func<TRequest, bool>>? predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult<TResponse>> GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult<TResponse>> GetByPredicate(Expression<Func<TRequest, bool>>? predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OperationResult<TResponse>> Update(int Id, TRequest requestModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
