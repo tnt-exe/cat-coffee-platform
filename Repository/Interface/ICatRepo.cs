6f5ac21c288b3be732062941c74898e755a95fdf
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-02-01 03:13:00 +0700

    feat: implement cat repository

--- Repository/Interface/ICatRepo.cs
+++ Repository/Interface/ICatRepo.cs
@@ -1,6 +1,14 @@
-﻿namespace Repository.Interface
+﻿using DAO.Helper;
+using DTO.CatDTO;
+
+namespace Repository.Interface
 {
     public interface ICatRepo
     {
+        Task<OperationResult<IEnumerable<CatDto>>> GetCats();
+        Task<OperationResult<CatDto>> GetCatById(int id);
+        Task<OperationResult<CatCreate>> CreateCat(CatCreate catCreate);
+        Task<OperationResult<CatUpdate>> UpdateCat(CatUpdate catUpdate);
+        Task<OperationResult<object>> DeleteCat(int id);
     }
 }
