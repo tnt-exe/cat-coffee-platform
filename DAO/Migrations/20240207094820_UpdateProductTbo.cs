d73d9383c5cee574935f6ec95012b2e1d113077e
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-02-07 16:51:49 +0700

    fix: add migrations for updating product tbo

---
+++ DAO/Migrations/20240207094820_UpdateProductTbo.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAO.Migrations
+{
+    public partial class UpdateProductTbo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Unit",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Unit",
+                table: "Products");
+        }
+    }
+}
