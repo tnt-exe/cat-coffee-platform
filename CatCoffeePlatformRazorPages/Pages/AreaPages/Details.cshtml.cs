38d1fbcd0ff328bc88dd555adc2c014aaa33cde9
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-02-13 02:40:04 +0700

    feat: add area pages

---
+++ CatCoffeePlatformRazorPages/Pages/AreaPages/Details.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using CatCoffeePlatformRazorPages.Common;
+using DTO.AreaDTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CatCoffeePlatformRazorPages.Pages.AreaPages
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ApiHelper _apiArea;
+
+        public DetailsModel()
+        {
+            _apiArea = new ApiHelper(ApiResources.Areas);
+        }
+
+        public AreaDto Area { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var area = await _apiArea.GetAsync<AreaDto>($"{id}");
+            if (area == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Area = area;
+            }
+            return Page();
+        }
+    }
+}
---
+++ CatCoffeePlatformRazorPages/Pages/AreaPages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using CatCoffeePlatformRazorPages.Common;
+using DTO.AreaDTO;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CatCoffeePlatformRazorPages.Pages.AreaPages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApiHelper _apiArea;
+
+        public IndexModel()
+        {
+            _apiArea = new ApiHelper(ApiResources.Areas);
+        }
+
+        public IEnumerable<AreaDto> Area { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            var areaList = await _apiArea
+                .GetAsync<IEnumerable<AreaDto>>();
+            if (areaList is not null)
+            {
+                Area = areaList;
+            }
+        }
+    }
+}
