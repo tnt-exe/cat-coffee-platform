57767518ad6e919f569522d065674fd96a19cc3e
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-02-13 14:31:14 +0700

    feat: add response body api side

---
+++ CatCoffeePlatformAPI/Common/ResponseBody.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace CatCoffeePlatformAPI.Common
+{
+    public class ResponseBody<T> where T : class
+    {
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string? Title { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public IEnumerable<string>? Errors { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public string? Token { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+        public T? Result { get; set; }
+    }
+}
