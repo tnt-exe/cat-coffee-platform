27923f2a10a1efe8d65c166a2d606d8d97533aa3
Thaiquocse <Thaiquocse@gmail.com>
2024-03-07 19:18:32 +0700

    Something new

---
+++ CatCoffeePlatformRazorPages/Pages/CoffeeShopPages/ShowAllCoffeeShop.cshtml.cs
@@ -0,0 +1,29 @@
+using CatCoffeePlatformRazorPages.Common;
+using DTO.CoffeeShopDTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CatCoffeePlatformRazorPages.Pages.CoffeeShopPages
+{
+    public class ShowAllCoffeeShopModel : PageModel
+    {
+        private readonly ApiHelper _apiShop;
+
+        public ShowAllCoffeeShopModel()
+        {
+            _apiShop = new ApiHelper(ApiResources.CoffeeShops);
+        }
+
+        public IEnumerable<CoffeeShopResponseDTO> CoffeeShop { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            var apiResponse = await _apiShop.GetAsync<ResponseBody<IEnumerable<CoffeeShopResponseDTO>>>();
+            var shopList = apiResponse!.Result;
+            if (shopList is not null)
+            {
+                CoffeeShop = shopList;
+            }
+        }
+    }
+}
