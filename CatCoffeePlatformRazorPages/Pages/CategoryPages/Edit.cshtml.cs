6394a5d8f082ec1382f8894a6dd98aa6e992efdf
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-03-13 06:40:47 +0700

    feat: add category pages and ui

---
+++ CatCoffeePlatformRazorPages/Pages/CategoryPages/Edit.cshtml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using CatCoffeePlatformRazorPages.Common;
+using DTO;
+
+namespace CatCoffeePlatformRazorPages.Pages.CategoryPages
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApiHelper _apiCategory;
+
+        public EditModel()
+        {
+            _apiCategory = new ApiHelper(ApiResources.Categories);
+        }
+
+        [BindProperty]
+        public CategoryUpdate Category { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var apiResponse = await _apiCategory.GetAsync<ResponseBody<CategoryUpdate>>($"{id}");
+            var category = apiResponse!.Result;
+            if (category == null)
+            {
+                return NotFound();
+            }
+            Category = category;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            bool result = await _apiCategory.PutAsync($"{Category.CategoryId}", Category);
+
+            if (!result)
+            {
+                return Page();
+            }
+
+            TempData["category-msg"] = "Update category success";
+            return RedirectToPage("./Index");
+        }
+    }
+}
