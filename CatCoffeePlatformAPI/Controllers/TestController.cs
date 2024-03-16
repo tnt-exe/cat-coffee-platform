5b484995defa4d56e0cab994b37569cd077168dd
khoaLe12 <112998676+khoaLe12@users.noreply.github.com>
2024-03-16 17:09:24 +0700

    check DI for dbcontext

---
+++ CatCoffeePlatformAPI/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using DAO.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatCoffeePlatformAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public TestController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetOk()
+        {
+            return Ok(_applicationDbContext.Users.ToList());
+        }
+    }
+}
