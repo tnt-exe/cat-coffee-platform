5425a754c033322331448f8f4e9fc05d0b54394f
tnt-exe <119075262+tnt-exe@users.noreply.github.com>
2024-03-16 13:45:07 +0700

    feat: implement create, update product pages

--- DTO/ProductDTO/ProductUpdate.cs
+++ DTO/ProductDTO/ProductUpdate.cs
@@ -1,10 +1,21 @@
-﻿namespace DTO.ProductDTO;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DTO.ProductDTO;
 
 public record ProductUpdate
 {
+    [Required]
     public string? ProductName { get; set; }
+
+    [Required]
     public decimal? Price { get; set; }
+
+    [Required]
     public int? Quantity { get; set; }
+
+    [Required]
     public string? Unit { get; set; }
+
+    [Required]
     public int? CategoryId { get; set; }
 }
